3e42538de49a265ac296e4581bb379fd0251aa84
Kaan Basaran <kaan@makersedgeoutlook.onmicrosoft.com>
2025-09-08 07:29:59 -0400

    latest web update

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("2b5f009a9c001714")
+/******/ 		__webpack_require__.h = () => ("4018193aef46ba34")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
