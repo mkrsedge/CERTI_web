163fd66357313876c987282f91eb7a897f72e30d
Bora Tosun <bora@makersedgeoutlook.onmicrosoft.com>
2026-04-03 19:50:14 +0300

    Intro account booking page update

--- lib/booking.ts
+++ lib/booking.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_BOOKING_URL = 'https://calendar.app.google/bBPikunCZbbzPqVHA'
+export const DEFAULT_BOOKING_URL = 'https://calendar.app.google/UXF4r9RFoiR8kP8L8'
 
 export const getBookingUrl = () => process.env.NEXT_PUBLIC_GOOGLE_SCHEDULER_URL || DEFAULT_BOOKING_URL
